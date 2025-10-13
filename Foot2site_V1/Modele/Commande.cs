a8934c12df8d6457c04f587406a78942c40b601f
OmarYaya <la187291@student.helha.be>
2025-10-13 17:16:04 +0200

    Ajout des 2 modeles : Commmande et LigneCommande

---
+++ Foot2site_V1/Modele/Commande.cs
@@ -0,0 +1,13 @@
+﻿namespace Foot2site_V1.Modele
+{
+    public class Commande
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double prixTotal { get; set; }
+
+        public Boolean Paye { get; set; }
+
+
+    }
+}
