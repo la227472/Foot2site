744e0ac2dac432ac0e7b8528a6050e2849cdbbdf
Khabacha Loay <la226247@student.helha.be>
2025-12-12 19:11:58 +0100

    Ajout de la NavBar complete selon le role de l'utilisateur connecté

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ConnectionService } from './Service/connection.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontEnd';
+
+  constructor(public connectionService: ConnectionService) {}
 }
